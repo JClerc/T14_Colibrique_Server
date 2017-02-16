ed8c4eea8e5a6e482fe2e61d91db4ae3520b210e
JClerc <jices25@gmail.com>
2017-02-16 22:49:14 +0100

    Fix user fixtures

---
+++ src/AppBundle/DataFixtures/ORM/LoadClientData.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadClientData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /** @var  ContainerInterface */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $clientManager = $this->container->get('fos_oauth_server.client_manager.default');
+        $client = $clientManager->createClient();
+        $client->setRedirectUris([]);
+        $client->setAllowedGrantTypes(['password']);
+        $clientManager->updateClient($client);
+    }
+
+    public function getOrder()
+    {
+        return 1;
+    }
+}
