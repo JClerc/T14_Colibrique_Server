17148930902d0cd0f0d5f7f5b599c35fed509272
JClerc <jices25@gmail.com>
2017-02-17 17:45:28 +0100

    Handle exceptions properly

---
+++ src/AppBundle/Controller/ExceptionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Exception;
+use FOS\RestBundle\Controller\FOSRestController;
+
+class ExceptionController extends FOSRestController
+{
+    public function showAction(Exception $exception)
+    {
+        $data = [
+            'error' => [
+                'code' => $exception->getCode(),
+                'message' => $exception->getMessage(),
+                'type' => get_class($exception),
+            ],
+        ];
+
+        return $this->handleView($this->view($data));
+    }
+}
