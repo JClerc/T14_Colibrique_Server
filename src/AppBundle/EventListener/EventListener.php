60308c657f83e9f13077e1a19171e641dc5a0ea0
JClerc <jices25@gmail.com>
2017-02-24 00:51:42 +0100

    Fix CORS headers

--- src/AppBundle/EventListener/EventListener.php
+++ src/AppBundle/EventListener/EventListener.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\EventListener;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 /**
  * Class EventListener
@@ -15,10 +17,42 @@ class EventListener
      */
     public function onKernelResponse(FilterResponseEvent $event)
     {
+        if ($event->getRequest()->getMethod() === 'OPTIONS') {
+            $event->setResponse(new Response());
+        }
+
         $event->getResponse()->headers->add(
             [
                 'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Credentials' => 'true',
+                'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
             ]
         );
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+
+        if ($request->headers->get('Access-Control-Request-Headers') === 'authorization'
+            && $request->headers->get('Access-Control-Request-Method') === 'GET'
+        ) {
+            $response = new Response();
+            $response->headers->add(
+                [
+                    'Access-Control-Allow-Origin' => '*',
+                    'Access-Control-Allow-Credentials' => 'true',
+                    'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+                ]
+            );
+
+            $event->setResponse($response);
+            $event->getResponse()->setStatusCode(200);
+        }
     }
 }
